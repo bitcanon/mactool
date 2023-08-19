2766ddf2af71b8484914375442ed388610894b14
Mikael Schultz <bitcanon@proton.me>
2023-08-19 17:34:19 +0200

    Implement extract command.

--- cmd/extract.go
+++ cmd/extract.go
@@ -22,17 +22,133 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
+	"strings"
 
+	"github.com/bitcanon/mactool/mac"
 	"github.com/spf13/cobra"
 )
 
+// extractAction extracts MAC addresses from the input string
+func extractAction(out io.Writer, s string) error {
+	// Extract MAC addresses from string
+	macs, err := mac.FindAllMacAddresses(s)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	// Print MAC addresses found in the input string
+	// to the output writer
+	for _, mac := range macs {
+		fmt.Fprintln(out, mac)
+	}
+
+	// No errors occurred
+	return nil
+}
+
+// processArgs processes the command line arguments
+// and extracts MAC addresses from the input string
+func processArgs(args []string) error {
+	// Extract MAC addresses from string
+	if err := extractAction(os.Stdout, strings.Join(args, " ")); err != nil {
+		return err
+	}
+
+	// No errors occurred
+	return nil
+}
+
+// processStdin processes the standard input (piped data)
+// and extracts MAC addresses from the input string
+func processStdin() error {
+	// Read all data from standard input
+	input, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		return err
+	}
+
+	// Extract MAC addresses from string
+	if err := extractAction(os.Stdout, string(input)); err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	// No errors occurred
+	return nil
+}
+
+// processInteractiveInput processes the interactive input
+// and extracts MAC addresses from the input string
+func processInteractiveInput() error {
+	// A string for the user input
+	var line string
+
+	// Create a scanner to read from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// Read each line from standard input as the user types.
+	// The loop will exit when the user presses Ctrl+D (Unix)
+	// or Ctrl+Z (Windows).
+	for scanner.Scan() {
+		line = scanner.Text()
+	}
+
+	// Check for errors that may have occurred while reading
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	// Extract MAC addresses from string and
+	// print them to standard output
+	if err := extractAction(os.Stdout, line); err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+// Example help text for the extract command
+const extractExample = `  mactool extract 00:00:5e:00:53:01
+  mactool extract 0000.5e00.5301 00:00:5e:00:53:01 0000-5e00-5301 00-00-5e-00-53-01
+  mactool extract First address 0000.5E00.5301, second address 00:00:5e:00:53:01, etc.
+  cat macs.txt | mactool extract
+
+Interactive mode:
+  mactool extract
+
+While operating in interactive mode, enter or paste the input string and then press
+Enter to proceed. To exit, use Ctrl+D (Unix) or Ctrl+Z (Windows), followed by Enter.`
+
+// Long help text for the extract command
+const extractLong = `Extract MAC addresses from the input string
+
+The command takes input in the form of command line arguments,
+standard input (piped data) or interactive input.`
+
 // extractCmd represents the extract command
 var extractCmd = &cobra.Command{
-	Use:   "extract",
-	Short: "A brief description of your command",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("extract called")
+	Use:     "extract [input]",
+	Short:   "Extract MAC addresses from the input string",
+	Long:    extractLong,
+	Example: extractExample,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if stat, _ := os.Stdin.Stat(); (stat.Mode() & os.ModeCharDevice) == 0 {
+			// Process data from pipe or redirection
+			return processStdin()
+		} else {
+			// Process command line arguments or interactive input
+			if len(args) == 0 {
+				return processInteractiveInput()
+			} else {
+				return processArgs(args)
+			}
+		}
 	},
 }
 
