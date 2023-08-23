53293f7a58e67ccb20279d3c8a2a62a975adddf8
Mikael Schultz <bitcanon@proton.me>
2023-08-23 17:35:54 +0200

    Don't config file path on every command.

--- cmd/root.go
+++ cmd/root.go
@@ -22,7 +22,6 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -89,7 +88,5 @@ func initConfig() {
 	viper.AutomaticEnv()
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
+	viper.ReadInConfig()
 }
